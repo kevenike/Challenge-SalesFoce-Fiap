f65793b4b7d06735f7295daa1fea8ebb1ab35de0
Keven Ike <kevenikepsilva@gmail.com>
2024-03-09 19:45:23 -0300

    subindo minha aplicação web

---
+++ src/app/Components/Header/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import "./Header.css";
+
+const Header = () => {
+    return(
+        <>
+            <header className="cabecalho">
+                   <nav className="menu">
+                     <ul className="links-de-navegacao">
+                        <li><Link className="link" href="/">Home</Link></li>
+                        <li><Link className="link" href="">Produtos</Link></li>
+                        <li><Link className="link" href="">Quem Somos</Link></li>
+                        <li><Link className="link" href="">FAQ</Link></li>
+                        <li><Link className="link" href="">Nosso Time</Link></li>
+                     </ul>
+                    </nav> 
+            </header>
+        </>
+    )
+}
+export default Header;
---
+++ src/app/Components/IconeFormulario/page.tsx
@@ -0,0 +1,11 @@
+import "./IconeFormulario.css";
+import Image from "next/image"; 
+
+function IconeFormulario() {
+    return (
+        <>
+        
+        </>
+    );
+}
+export default IconeFormulario
