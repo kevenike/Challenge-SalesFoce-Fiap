f38328fe0456a2090355da92cb92177efeb12312
Keven Ike <kevenikepsilva@gmail.com>
2024-03-29 11:28:43 -0300

    adiconando midia para as paginas

--- src/app/Components/Header/page.tsx
+++ src/app/Components/Header/page.tsx
@@ -19,10 +19,10 @@ const Header = () => {
                     </div>
                      <ul className="links-de-navegacao">
                         <li><Link className="link link-cor-preta" href="/">Home</Link></li>
-                        <li><Link className="link link-cor-preta" href="./Pages/Produtos">Produtos</Link></li>
-                        <li><Link className="link link-cor-preta" href="./Pages/QuemSomos">Quem Somos</Link></li>
-                        <li><Link className="link link-cor-preta" href="">FAQ</Link></li>
-                        <li><Link className="link link-cor-preta" href="">Nosso Time</Link></li>
+                        <li><Link className="link link-cor-preta" href="/Pages/Produtos">Produtos</Link></li>
+                        <li><Link className="link link-cor-preta" href="/Pages/QuemSomos">Quem Somos</Link></li>
+                        <li><Link className="link link-cor-preta" href="/Pages/Faq">FAQ</Link></li>
+                        <li><Link className="link link-cor-preta" href="/Pages/NossoTime">Nosso Time</Link></li>
                      </ul>
                     </nav> 
             </header>
