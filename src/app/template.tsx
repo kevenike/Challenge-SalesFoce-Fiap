2b8aca72356a4a67fb061b55faa561651de3254e
Keven Ike <kevenikepsilva@gmail.com>
2024-05-21 00:43:16 -0300

    conexão com java

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,9 +1,9 @@
 "use client"
-import React from "react"; 
-import {motion} from "framer-motion"
+import React from "react";
+import { motion } from "framer-motion"
 
-export default function Templete({children}: {children: React.ReactNode}){
-    return <motion.div initial={{y: 16, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{ease: 'easeInOut', duration:0.75}}>
+export default function Templete({ children }: { children: React.ReactNode }) {
+    return <motion.div initial={{ x: -16, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ ease: 'easeInOut', duration: 0.75 }}>
         {children}
     </motion.div>;
 }
